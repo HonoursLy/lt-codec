156dce4c25c252b8c1b7fb3134d28590d3ffd44a
Henry Chen <henrychen385@gmail.com>
2025-09-26 17:50:40 +0800

    Using Yosys BRAM block to synthesise RAM.

--- vhdl/TX_RAM.vhd
+++ vhdl/TX_RAM.vhd
@@ -1,92 +1,91 @@
 -- Library and Use statements for IEEE packages
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use IEEE.std_logic_signed.all;
-
-
-entity TX_RAM is
-	port (
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE IEEE.std_logic_signed.ALL;
+ENTITY TX_RAM IS
+	PORT (
 		-- enter port declarations here
-		wr_clk : in STD_LOGIC;
-		rd_clk : in STD_LOGIC;
-		reset : in STD_LOGIC;
-		wr_en : in STD_LOGIC;
-		rd_en : in STD_LOGIC;
-		wr_data : in STD_LOGIC_VECTOR (9 downto 0);
-		rd_data : out STD_LOGIC_VECTOR (9 downto 0);
-		tx_length : in STD_LOGIC_VECTOR (10 downto 0)
+		wr_clk : IN STD_LOGIC;
+		rd_clk : IN STD_LOGIC;
+		reset : IN STD_LOGIC;
+		wr_en : IN STD_LOGIC;
+		rd_en : IN STD_LOGIC;
+		wr_data : IN STD_LOGIC_VECTOR (9 DOWNTO 0);
+		rd_data : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+		tx_length : IN STD_LOGIC_VECTOR (10 DOWNTO 0)
 	);
-end entity TX_RAM;
+END ENTITY TX_RAM;
 
-architecture arch of TX_RAM is
-	SIGNAL w_count : STD_LOGIC_VECTOR (10 downto 0) := "00000000000";
-	SIGNAL r_count : STD_LOGIC_VECTOR (10 downto 0) := "00000000000";
-	SIGNAL wr_addr_i : STD_LOGIC_VECTOR (10 downto 0);
-	SIGNAL rd_addr_i : STD_LOGIC_VECTOR (10 downto 0);
+ARCHITECTURE arch OF TX_RAM IS
+	SIGNAL w_count : STD_LOGIC_VECTOR (10 DOWNTO 0) := "00000000000";
+	SIGNAL r_count : STD_LOGIC_VECTOR (10 DOWNTO 0) := "00000000000";
+	SIGNAL wr_addr_i : STD_LOGIC_VECTOR (10 DOWNTO 0);
+	SIGNAL rd_addr_i : STD_LOGIC_VECTOR (10 DOWNTO 0);
 
-	component ram is
-		generic (
-			addr_width : natural := 9; -- 512x8
-			data_width : natural := 8
+	COMPONENT bram_2048x10
+		PORT (
+			-- Write port
+			wclk : IN STD_LOGIC;
+			we : IN STD_LOGIC;
+			waddr : IN STD_LOGIC_VECTOR(10 DOWNTO 0); -- 2048 deep
+			din : IN STD_LOGIC_VECTOR(9 DOWNTO 0);
+
+			-- Read port
+			rclk : IN STD_LOGIC;
+			re : IN STD_LOGIC;
+			raddr : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+			dout : OUT STD_LOGIC_VECTOR(9 DOWNTO 0)
 		);
-		port (
-			write_en : in  std_logic;
-			waddr    : in  std_logic_vector (addr_width - 1 downto 0);
-			wclk     : in  std_logic;
-			raddr    : in  std_logic_vector (addr_width - 1 downto 0);
-			rclk     : in  std_logic;
-			din      : in  std_logic_vector (data_width - 1 downto 0);
-			dout     : out std_logic_vector (data_width - 1 downto 0)
-		);
-	end component;
+	END COMPONENT;
 
+BEGIN
 
-    begin
-
-process (wr_clk, reset)
-	begin
-		if (reset = '1') then
-			w_count <= (others => '0');
-		elsif falling_edge(wr_clk) then
-			if wr_en = '1' then
-				if (w_count = tx_length) then
-					w_count <= (others => '0');
-				else
+	PROCESS (wr_clk, reset)
+	BEGIN
+		IF (reset = '1') THEN
+			w_count <= (OTHERS => '0');
+		ELSIF falling_edge(wr_clk) THEN
+			IF wr_en = '1' THEN
+				IF (w_count = tx_length) THEN
+					w_count <= (OTHERS => '0');
+				ELSE
 					w_count <= w_count + 1;
-				end if;
-			end if;
-		end if;
-	end process;
-process (rd_clk, reset)
-	begin
-		if (reset = '1') then
-			r_count <= (others => '0');
-		elsif falling_edge(rd_clk) then
-			if rd_en = '1' then
-				if (r_count = tx_length) then
-					r_count <= (others => '0');
-				else
+				END IF;
+			END IF;
+		END IF;
+	END PROCESS;
+	PROCESS (rd_clk, reset)
+	BEGIN
+		IF (reset = '1') THEN
+			r_count <= (OTHERS => '0');
+		ELSIF falling_edge(rd_clk) THEN
+			IF rd_en = '1' THEN
+				IF (r_count = tx_length) THEN
+					r_count <= (OTHERS => '0');
+				ELSE
 					r_count <= r_count + 1;
-				end if;
-			end if;
-		end if;
-	end process;
+				END IF;
+			END IF;
+		END IF;
+	END PROCESS;
 
-	RAM_TX : component ram
-	generic map(
-		addr_width => 11, -- 2048x10
-		data_width => 10
-	)
-	port map(
-		write_en => wr_en,
-		waddr => wr_addr_i,
-		wclk => wr_clk,
-		raddr => rd_addr_i, 
-		rclk => rd_clk,
-		din => wr_data,
-		dout => rd_data
-	);
-	wr_addr_i <= w_count;
-	rd_addr_i <= r_count;
-end architecture arch;
+	RAM_TX : COMPONENT ram
+		GENERIC MAP(
+			addr_width => 11, -- 2048x10
+			data_width => 10
+		)
+		PORT MAP(
+			wclk => wr_clk,
+			we => wr_en,
+			waddr => wr_addr_i,
+			din => wr_data,
+			rclk => rd_clk,
+			re => rd_en,
+			raddr => rd_addr_i,
+			dout => rd_data
+		);
+
+		wr_addr_i <= w_count;
+		rd_addr_i <= r_count;
+	END ARCHITECTURE arch;
